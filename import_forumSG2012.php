7f5ab9d29d1eb2f6323deea6cf8fd39b68c03693
ashfack <abdoulazid.ashfack@gmail.com>
2016-03-22 22:05:12 +0100

    import - partie excel forum SG

---
+++ import_forumSG2012.php
@@ -0,0 +1,42 @@
+<?php
+	require_once 'Classes/PHPExcel/IOFactory.php'; 
+	header('Content-Type: text/html; charset=utf-8');
+
+	try 
+	{
+		require('db_connect.php');
+	    echo "Connected successfully";
+	}
+	catch(PDOException $e)
+	{
+	    echo "Connection failed: " . $e->getMessage();
+	}
+	// Chargement du fichier Excel
+	$objPHPExcel = PHPExcel_IOFactory::load("files/MAITRE_FEE_2012.xlsx");
+	 
+	$sheet = $objPHPExcel->getSheet(0);
+	$objWorksheet = $objPHPExcel->getActiveSheet();
+
+	$highestRow = $objWorksheet->getHighestRow(); 
+
+	$nomEntreprise="";
+	$commentaire="";
+	//echo "<li> highestRow = $highestRow </li>";
+	for ($row = 2; $row <= $highestRow; ++$row) 
+	{
+	  	//echo '<tr>';
+	  	$nomEntreprise=$objWorksheet->getCell('A'.$row)->getCalculatedValue();
+		try
+		{
+			$req = $conn->prepare("INSERT INTO ForumSG (anneeDeParticipation,Entreprise_nomEntreprise)VALUES (2012,:nomEntreprise)");
+				$req->execute(array(
+				    'nomEntreprise' => $id,
+				 ));
+				//echo "<li> Ajout de $id $cycle $mention $specialite </li>";
+		}
+		catch(PDOException $e)
+		{
+		    echo "<li>errrrrrrrrrrrrrreur sql: " . $e->getMessage()."</li>";
+		}
+	}	
+?>
